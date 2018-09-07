f88780a6caf8b41ef1771130fad78b69de46c518
Marco García <marcogarcia.gon@gmail.com>
2018-09-07 13:07:35 -0500

    Actualización de menu

--- assets/base/js/scripts.js
+++ assets/base/js/scripts.js
@@ -1,6 +1,17 @@
 /*Gathering id*/
 var get=document.getElementById("depas");
-
-function depaA () {
-	get.src='assets/base/img/content/modelos/planta-a_web.jpg';
+function depaA() {
+get.src='assets/base/img/content/modelos/planta-a_web.jpg';
+}
+function depaB() {
+get.src='assets/base/img/content/modelos/planta-b_web.jpg';
+}
+function depaC() {
+get.src='assets/base/img/content/modelos/planta-c_web.jpg';
+}
+function depaD() {
+get.src='assets/base/img/content/modelos/planta-d_web.jpg';
+}
+function depaE() {
+get.src='assets/base/img/content/modelos/planta-e_web.jpg';
 }
